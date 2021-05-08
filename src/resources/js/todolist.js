fafc8219653c08159aabc6daca7db4e5c3e65e29
丘惟夫 <waveciou@gmail.com>
2021-05-08 17:32:38 +0800

    todolist practice

--- src/resources/js/todolist.js
+++ src/resources/js/todolist.js
@@ -2,7 +2,7 @@ import '../scss/main.scss';
 import '../scss/todolist.scss';
 
 (function(Vue) {
-  const { ref, reactive } = Vue;
+  const { ref, reactive, computed } = Vue;
 
   const app = {
     template: `
@@ -11,16 +11,37 @@ import '../scss/todolist.scss';
           <input
             class="todolist__input"
             type="text"
-            v-model="inputValue"
+            placeholder="Please enter your todo item"
+            v-model.trim="inputValue"
           >
           <button
-            class="submit-btn"
+            class="btn submit-btn"
             @click.stop="addTodoHandler"
-          >submit</button>
+          >Add Todo</button>
         </div>
         <div class="todolist__body">
+          <div
+            v-if="todolist.length > 0"
+            class="todolist__types-control"
+          >
+            <button
+              class="btn select-btn"
+              :class="{'current': todoType !== 0 && todoType !== 1}"
+              @click.stop="todoType = 2"
+            >ALL</button>
+            <button
+              class="btn select-btn"
+              :class="{'current': todoType === 1}"
+              @click.stop="todoType = 1"
+            >CHECKED</button>
+            <button
+              class="btn select-btn"
+              :class="{'current': todoType === 0}"
+              @click.stop="todoType = 0"
+            >UNCHECKED</button>
+          </div>
           <ul class="todolist__list">
-            <li v-for="todoItem of todolist" :key="todoItem.id">
+            <li v-for="todoItem of todolistByTypes" :key="todoItem.id">
               <div class="todolist__item">
                 <div class="todolist__checkbox">
                   <input
@@ -32,6 +53,10 @@ import '../scss/todolist.scss';
                   <label :for="todoItem.id"></label>
                 </div>
                 <div class="todolist__title">{{ todoItem.value }}</div>
+                <button
+                  class="delete-btn"
+                  @click.stop="deleteTodoHandler(todoItem.id)"
+                ></button>
               </div>
             </li>
           </ul>
@@ -41,6 +66,7 @@ import '../scss/todolist.scss';
     setup() {
       const inputValue = ref('');
       const todolist = reactive([]);
+      const todoType = ref('2');
 
       const addTodoHandler = () => {
         if (inputValue.value === '') {
@@ -51,18 +77,41 @@ import '../scss/todolist.scss';
         const date = new Date();
 
         todolist.push({
-          id: `${date.getTime()}`,
+          id: `todoitem_${date.getTime()}`,
           value: inputValue.value,
           checked: false
         });
 
         inputValue.value = '';
+        todoType.value = 2;
       };
+
+      const deleteTodoHandler = (id) => {
+        const index = todolist.findIndex(element => element.id === id);
+        if (index < 0) return false;
+        todolist.splice(index, 1);
+      };
+
+      const todolistByTypes = computed(() => {
+        const type = todoType.value;
+        return todolist.filter(todoItem => {
+          if (type === 0) {
+            return todoItem.checked === false;
+          } else if (type === 1) {
+            return todoItem.checked === true;
+          } else {
+            return todoItem;
+          }
+        });
+      });
 
       return {
         inputValue,
         todolist,
-        addTodoHandler
+        todoType,
+        addTodoHandler,
+        deleteTodoHandler,
+        todolistByTypes
       };
     }
   };
