68cbb189cfaa25081e3ba87c97a9973df680edb6
丘惟夫 <waveciou@gmail.com>
2022-06-08 22:22:22 +0800

    [dev] edit vite html config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,44 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 import vue from '@vitejs/plugin-vue';
 import eslintPlugin from 'vite-plugin-eslint';
+import { createHtmlPlugin } from 'vite-plugin-html';
+
+const root: string = resolve(__dirname, 'src');
+const outDir: string = resolve(__dirname, 'dist');
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), eslintPlugin()],
+  root,
+  base: '/vue3-practice/',
+  plugins: [
+    vue(),
+    eslintPlugin(),
+    createHtmlPlugin({
+      minify: true,
+    }),
+  ],
+  build: {
+    outDir,
+    assetsInlineLimit: 0,
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: 'js/main.js',
+        chunkFileNames: 'js/[name].js',
+        assetFileNames: ({ name = '' }) => {
+          if (/\.(gif|jpe?g|png|svg)$/.test(name)) {
+            return 'img/[name].[ext]';
+          }
+          if (/\.css$/.test(name)) {
+            return 'css/[name].[ext]';
+          }
+          if (/manifest\.json$/.test(name)) {
+            return '[name].[ext]';
+          }
+          return '[name].[ext]';
+        },
+      },
+    },
+  },
 });
